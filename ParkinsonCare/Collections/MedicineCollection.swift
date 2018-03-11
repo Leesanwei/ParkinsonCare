5f34871022d901e23aac209de363ec884de5ecb1
Loïc Combis <combis.loic@gmail.com>
2018-03-11 15:55:15 +0100

    deleted managers and facades

---
+++ ParkinsonCare/Collections/MedicineCollection.swift
@@ -0,0 +1,31 @@
+//
+//  MedicineCollection.swift
+//  ParkinsonCare
+//
+//  Created by Loic Combis on 11/03/2018.
+//  Copyright © 2018 San-Wei LEE. All rights reserved.
+//
+
+import Foundation
+
+class MedicineCollection{
+    
+    
+    func push(_newMedicine medicine : Medicine) -> MedicineCollection {
+        return self
+    }
+    
+    func remove(_medicineToRemove medicine : Medicine) -> MedicineCollection {
+        return self
+    }
+    
+    func remove(_byName name : String) -> MedicineCollection{
+        return self
+    }
+    
+    func find(_byName name : String) -> Medicine?{
+        return nil
+    }
+    
+    
+}
