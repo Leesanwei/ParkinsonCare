2e9d8e5e26bf98ebf3722f791f98e2870df471b9
admin <admin@projets-imac.home>
2018-03-17 23:46:28 +0100

    test

---
+++ ParkinsonCare/Controllers/MedecinSportsController.swift
@@ -0,0 +1,23 @@
+//
+//  MedecinSportsController.swift
+//  ParkinsonCare
+//
+//  Created by admin on 15/03/2018.
+//  Copyright © 2018 San-Wei LEE. All rights reserved.
+//
+
+import UIKit
+
+class MedecinSportsController: UIViewController{
+    
+    @IBOutlet weak var textField : UITextField
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+}
